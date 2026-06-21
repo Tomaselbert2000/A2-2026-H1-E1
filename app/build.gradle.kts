61975a26e470e97bf7b9b71f4316dbd372435787
Aguirre <AmAguirre@emergencias.com.ar>
2026-06-21 16:23:00 -0300

    features: post api datasource and post component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
 
     // Dagger + Hilt
     implementation(libs.google.dagger.hilt.android)
